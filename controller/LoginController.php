810da0ef340b2250f44c3a79e0afcf7802d7dd3c
Yannis-Alouache <alouacehyannis1@gmail.com>
2024-01-07 16:29:14 +0100

    [+] added login

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -5,6 +5,8 @@ include_once("./controller/Controller.php");
 include_once("view/navigation.php");
 include_once("view/footer.php");
 include_once("view/LoginPage.php");
+include_once("model/UserModel.php");
+
 
 class LoginController extends Controller
 {
@@ -13,9 +15,8 @@ class LoginController extends Controller
     public LoginPage $loginPage;
 
     private const ROUTES = array(
-        "hey@GET" => "testGet",
-        "hey@POST" => "testPost",
         "@GET" => "render",
+        "@POST" => "doLogin",
     );
 
 	public function getInnerRoutes(): array
@@ -27,6 +28,41 @@ class LoginController extends Controller
         $this->navigation = new Navigation();
         $this->loginPage = new LoginPage();
         $this->footer = new Footer();
+    }
+
+    public function validateEmail($email) {
+        return preg_match('/^[\w-]+(\.[\w-]+)*@([\w-]+\.)+[a-zA-Z]{2,7}$/', $email) === 1;
+    }
+
+    public function doLogin() {
+        $email = $_POST["email"];
+        $password = $_POST["password"];
+
+        if ( !$this->validateEmail($email) )
+        {
+            return $this->render(array("error" => "Vous devez saisir une adresse mail valide."));
+        }
+
+        $user = new UserModel();
+
+        $userData = $user
+            ->where("email", $email, "=")
+            ->get();
+
+        
+        if (password_verify($password, $userData->motdepasse)) {
+            if (session_status() === PHP_SESSION_NONE)
+                session_start();
+            $_SESSION["userId"] = $userData->idUtilisateur;
+            $_SESSION["userFullName"] = $userData->nom . " " . $userData->prenom;
+            $_SESSION["userEmail"] = $userData->email;
+            $_SESSION["userAdmin"] = $userData->admin;
+            return $this->render(array("success" => "Vous êtes connecté !"));
+        } else {
+            return $this->render(array("error" => "Information de connexion incorrect !"));
+        }
+
+
     }
 
     public function render($context = []) {
