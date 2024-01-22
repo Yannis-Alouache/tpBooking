682c06d3f33b8dccebd78a917293c1638c04e17f
Yannis-Alouache <alouacehyannis1@gmail.com>
2024-01-22 14:37:48 +0100

    [+] refractoring

--- view/LoginPage.php
+++ view/LoginPage.php
@@ -3,6 +3,29 @@
 include_once("Template.php");
 
 class LoginPage extends Template {
+
+    public function loginForm() : string {
+        return '
+            <form class="space-y-4 md:space-y-6" action="/login" method="POST">
+                <div>
+                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
+                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                </div>
+                <div>
+                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
+                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
+                </div>
+                <div class="flex items-center justify-between">
+                    <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-white">mot de passe oublié ?</a>
+                </div>
+                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bg-blue-600">Connexion</button>
+                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
+                    Pas encore de compte ? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-blue-500">Je veux en créer un !</a>
+                </p>
+            </form>
+        ';
+    }
+
     public function render($context) : string {
 
         $html = '
@@ -30,31 +53,15 @@ class LoginPage extends Template {
                                     $context["success"] .
                                 '</strong></div>';
                             
-                                $html .= '
-
-                            <form class="space-y-4 md:space-y-6" action="/login" method="POST">
-                                <div>
-                                    <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email</label>
-                                    <input type="email" name="email" id="email" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="name@company.com" required="">
+                            $html .= $this->loginForm();     
+                        
+                            $html .= '
                                 </div>
-                                <div>
-                                    <label for="password" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Mot de passe</label>
-                                    <input type="password" name="password" id="password" placeholder="••••••••" class="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required="">
-                                </div>
-                                <div class="flex items-center justify-between">
-                                    <a href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-white">mot de passe oublié ?</a>
-                                </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800 bg-blue-600">Connexion</button>
-                                <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Pas encore de compte ? <a href="#" class="font-medium text-primary-600 hover:underline dark:text-blue-500">Je veux en créer un !</a>
-                                </p>
-                            </form>
+                            </div>
                         </div>
-                    </div>
-                </div>
-            </section>
-        ';
-    
+                    </section>
+                    ';
+                    
         return $html;
     }
 }
